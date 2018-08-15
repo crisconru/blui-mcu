c4e9d40986aa7e564ae54d01dfb28b7ff4388e7e
Cristo <crisconru@gmail.com>
2018-08-15 23:35:40 +0200

    Ya funciona todo

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <HC06.h>
+#include <HC06.hpp>
 
 // HC05
 #define HC05_EN  13
@@ -10,17 +10,17 @@
 // HC06
 #define HC06_TX  14
 #define HC06_RX  15
-HC06 hc06();
+HC06 hc06(HC06_TX, HC06_RX);
 // Variables
 String msg;
 // Funciones
 String leer_serial();
-char end_char = '\n';
+char end_char = 'x';
 
 void setup() {
     Serial.begin(9600);
-    hc06.set_tx(HC06_TX);
-    hc06.set_rx(HC06_RX);
+    //hc06.set_tx(HC06_TX);
+    //hc06.set_rx(HC06_RX);
     hc06.begin(9600);
     Serial.println("Enviar comandos AT");
 }
@@ -33,8 +33,10 @@ void loop() {
     if(Serial.available()){
         msg = leer_serial();
         Serial.println("Serial -> " + msg);
+        hc06.write(msg);
     }
     delay(15);
+    msg = "";
 }
 
 String leer_serial(){
