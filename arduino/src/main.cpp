d9ba9669add0eb672e8f9e025968287b54a1f806
Cristo <crisconru@gmail.com>
2018-08-15 23:16:20 +0200

    HC06 lib creado pero da error para probarlo en el main, no detecta sus metodos

---
+++ arduino/src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <HC06.h>
+
+// HC05
+#define HC05_EN  13
+#define HC05_VCC 12
+#define HC05_TX  11
+#define HC05_RX  10
+#define HC05_ST   9
+// HC06
+#define HC06_TX  14
+#define HC06_RX  15
+HC06 hc06();
+// Variables
+String msg;
+// Funciones
+String leer_serial();
+char end_char = '\n';
+
+void setup() {
+    Serial.begin(9600);
+    hc06.set_tx(HC06_TX);
+    hc06.set_rx(HC06_RX);
+    hc06.begin(9600);
+    Serial.println("Enviar comandos AT");
+}
+
+void loop() {
+    msg = hc06.read();
+    if(msg != ""){
+        Serial.println("BT -> " + msg);
+    }
+    if(Serial.available()){
+        msg = leer_serial();
+        Serial.println("Serial -> " + msg);
+    }
+    delay(15);
+}
+
+String leer_serial(){
+    String m = "";
+    char in_char;
+    while(Serial.available()){
+        in_char = (char)Serial.read();
+        if(in_char != end_char){
+            m += in_char;
+        } else {
+            break;
+        }
+    }
+    return m;
+}
